1451fcf79fbd6a231b8620fca75fae3d272ea07a
ROBERT NAJIB LANSANGAN (OPS-IRIS-ISD-OOCLL/MNL) <LANSARO@OOCL.COM>
2019-10-10 12:24:53 +0800

    Small Commit Story one

--- src/main/java/iPhone.java
+++ src/main/java/iPhone.java
@@ -1,4 +1,18 @@
 public class iPhone extends Mobile{
+    private String message;
 
-    
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void call() {
+        System.out.println("<Iphone>"+ "Message :" + message);
+    }
 }
